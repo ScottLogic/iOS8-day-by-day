fb12252eed097971c46bca51f44fb761bec8e673
Sam Davies <sdavies@shinobicontrols.com>
2014-06-13 14:42:15 +0100

    Fixes some indentation

--- 02-sharing-extension/ShareAlike/ShareExtension/ShareViewController.swift
+++ 02-sharing-extension/ShareAlike/ShareExtension/ShareViewController.swift
@@ -10,22 +10,22 @@ import UIKit
 import Social
 
 class ShareViewController: SLComposeServiceViewController {
-
-    override func isContentValid() -> Bool {
-        // Do validation of contentText and/or NSExtensionContext attachments here
-        return true
-    }
-
-    override func didSelectPost() {
-        // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
+  
+  override func isContentValid() -> Bool {
+    // Do validation of contentText and/or NSExtensionContext attachments here
+    return true
+  }
+  
+  override func didSelectPost() {
+    // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
     
-        // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
-        self.extensionContext.completeRequestReturningItems(nil, completionHandler: nil)
-    }
-
-    override func configurationItems() -> AnyObject[]! {
-        // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
-        return NSArray()
-    }
-
+    // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
+    self.extensionContext.completeRequestReturningItems(nil, completionHandler: nil)
+  }
+  
+  override func configurationItems() -> AnyObject[]! {
+    // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
+    return NSArray()
+  }
+  
 }
