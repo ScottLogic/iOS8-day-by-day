db5f11ea3361d4423434da4124225ea88b34a81c
Sam Davies <sdavies@shinobicontrols.com>
2014-06-13 13:51:01 +0100

    Created ShareAlike project
    This will form the basis of the sharing-extension demo

---
+++ 02-sharing-extension/ShareAlike/ShareAlike/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShareAlike
+//
+//  Created by Sam Davies on 13/06/2014.
+//  Copyright (c) 2014 ShinobiControls. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
