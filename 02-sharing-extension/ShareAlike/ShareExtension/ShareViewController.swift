097c8050cdfa00e3a2c024da52a09e8b936237ec
Sam Davies <sdavies@shinobicontrols.com>
2014-06-24 11:47:09 +0100

    Shelving this until beta3 is out :[
    Debugging doesn't really work at the moment, which makes it very hard to
    build something without docs...

--- 02-sharing-extension/ShareAlike/ShareExtension/ShareViewController.swift
+++ 02-sharing-extension/ShareAlike/ShareExtension/ShareViewController.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 import Social
+import MobileCoreServices
+import CoreGraphics
+
 
 class ShareViewController: SLComposeServiceViewController {
   
@@ -18,14 +21,53 @@ class ShareViewController: SLComposeServiceViewController {
   
   override func didSelectPost() {
     // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
+    let configName = "com.shinobicontrols.ShareAlike.BackgroundSessionConfig"
+    let sessionConfig = NSURLSessionConfiguration.backgroundSessionConfiguration(configName)
+    let session = NSURLSession(configuration: sessionConfig)
+    
+    println(extensionContext.inputItems)
+    println("HELLLLOOOOOO")
+    
+    // Only interested in the first item
+    let extensionItem = extensionContext.inputItems[0] as NSExtensionItem
+    
+    let picDeets = pictureDetailsFromExtensionItem(extensionItem)
+    
+    // Prepare the URL Request
+    let request = urlRequestWithExtensionItem(extensionItem, text: "hello")
     
     // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
-    self.extensionContext.completeRequestReturningItems(nil, completionHandler: nil)
+    extensionContext.completeRequestReturningItems(nil, completionHandler: nil)
   }
   
   override func configurationItems() -> AnyObject[]! {
     // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
     return NSArray()
+  }
+  
+  
+  func urlRequestWithExtensionItem(extensionItem: NSExtensionItem, text: String) -> NSURLRequest? {
+    let url = NSURL.URLWithString("http://requestb.in/15tnugc1")
+    let request = NSMutableURLRequest(URL: url)
+    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+    request.addValue("application/json", forHTTPHeaderField: "Accept")
+    request.HTTPMethod = "POST"
+    return nil
+  }
+  
+  func pictureDetailsFromExtensionItem(extensionItem: NSExtensionItem) -> Dictionary<String, String> {
+    
+    for attachment in extensionItem.attachments as NSItemProvider[] {
+      if(attachment.hasItemConformingToTypeIdentifier(kUTTypeImage)) {
+        attachment.loadItemForTypeIdentifier(kUTTypeImage, options: nil,
+          completionHandler: {
+            (image: NSSecureCoding!, error: NSError!) in
+            let castedImage = image as UIImage
+            println(castedImage)
+          })
+      }
+    }
+    return ["picture": "hi"]
   }
   
 }
