4d94c6835029e9b41a3d99f3109546272968ffda
Sam Davies <sdavies@shinobicontrols.com>
2014-06-27 21:36:54 +0100

    Creates an async test which checks that the horse race callbacks work
    It's easy to make it fail by changing the duration of the test or the
    race

--- 11-async-testing/HorseRace/HorseRaceTests/HorseRaceTests.swift
+++ 11-async-testing/HorseRace/HorseRaceTests/HorseRaceTests.swift
@@ -7,29 +7,51 @@
 //
 
 import XCTest
+import HorseRace
+import UIKit
 
 class HorseRaceTests: XCTestCase {
+  
+  var viewController: ViewController!
+  
+  override func setUp() {
+    super.setUp()
+    // Put setup code here. This method is called before the invocation of each test method in the class.
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    // Get hold of the view controller
+    let window = UIApplication.sharedApplication().delegate.window!
+    viewController = window.rootViewController as? ViewController
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
+  }
+  
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    super.tearDown()
+  }
+  
+  
+  func testRaceCallbacks() {
+    // The horse race controller should callback each time a horse completes
+    // the race.
+    let raceController = viewController.createRaceController()
     
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
+    let h1Expectation = expectationWithDescription("Horse 1 should complete")
+    let h2Expectation = expectationWithDescription("Horse 2 should complete")
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
-    }
+    raceController.startRace(3, horseCrossedLineCallback: {
+      (horse: Horse) in
+      switch horse.horseView {
+      case self.viewController.horse1:
+        h1Expectation.fulfill()
+      case self.viewController.horse2:
+        h2Expectation.fulfill()
+      default:
+        XCTFail("Completetion called with unknown horse")
+      }
+    })
     
+    waitForExpectationsWithTimeout(5, handler: nil)
+  }
+  
+  
 }
