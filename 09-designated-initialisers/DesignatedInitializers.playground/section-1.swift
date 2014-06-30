d7bc7390a1cf85b2c74b889fe6fab3245215e27b
Sam Davies <sdavies@shinobicontrols.com>
2014-06-30 13:33:54 +0100

    Completes a simple swift playground which demonstrates initialisation
    There are a couple of classes, Person and Ninja. Might need some more
    work when the writeup begins

--- 09-designated-initialisers/DesignatedInitializers.playground/section-1.swift
+++ 09-designated-initialisers/DesignatedInitializers.playground/section-1.swift
@@ -2,4 +2,62 @@
 
 import Cocoa
 
-var str = "Hello, playground"
+class Person {
+  var name: String
+  var age: Int?
+  var consideredDangerous = false
+  
+  init(name: String, age: Int?) {
+    self.name = name
+    self.age = age
+  }
+  
+  convenience init(name: String) {
+    self.init(name: name, age: nil)
+  }
+  
+}
+
+enum Weapon {
+  case Katana, Tsuba, Shuriken, Kusarigama, Fukiya
+}
+
+class Ninja: Person {
+  var weapons: Weapon[]?
+  
+  init(name: String, age: Int?, weapons: Weapon[]?) {
+    self.weapons = weapons
+    
+    super.init(name: name, age: age)
+    
+    self.consideredDangerous = true
+  }
+  
+  convenience init(name: String) {
+    self.init(name: name, age: nil, weapons: nil)
+  }
+}
+
+
+let tony = Person(name: "tony")
+tony.consideredDangerous
+tony.age
+tony.age = 43
+tony
+
+
+let tara = Person(name: "tara", age: 27)
+tara.consideredDangerous
+tara.age
+
+
+let trevor = Ninja(name: "trevor")
+trevor.age
+trevor.consideredDangerous
+
+let tina = Ninja(name: "tina", age: 23, weapons: [.Fukiya, .Tsuba])
+tina.consideredDangerous
+tina.weapons
+tina.age
+
+
