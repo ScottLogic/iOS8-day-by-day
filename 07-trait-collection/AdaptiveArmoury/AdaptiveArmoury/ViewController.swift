52872d7f695abf0e26c5211966f79a9a0c9c5acb
Sam Davies <sdavies@shinobicontrols.com>
2014-06-30 14:55:46 +0100

    Creates a simple single-page app for demonstrating sizing traits

---
+++ 07-trait-collection/AdaptiveArmoury/AdaptiveArmoury/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AdaptiveArmoury
+//
+//  Created by Sam Davies on 30/06/2014.
+//  Copyright (c) 2014 ShinobiControls. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
