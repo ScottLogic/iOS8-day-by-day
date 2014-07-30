fde30f43f8fda1831d554c19eb5b6764b372a2fb
Sam Davies <sdavies@shinobicontrols.com>
2014-07-30 17:10:20 +0100

    A spot of refactoring
    All the CoreImage guff is in it's own class so that the detection code
    is all in a short view controller class

--- 13-coreimage-detectors/LiveDetection/LiveDetection/ViewController.swift
+++ 13-coreimage-detectors/LiveDetection/LiveDetection/ViewController.swift
@@ -14,70 +14,26 @@
 //
 
 import UIKit
-import GLKit
-import AVFoundation
-import CoreMedia
-import CoreImage
-import OpenGLES
-import QuartzCore
 
-class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
-                            
-  var videoDisplayView: GLKView!
-  var videoDisplayViewBounds: CGRect!
-  var renderContext: CIContext!
+class ViewController: UIViewController {
   
-  var avSession: AVCaptureSession!
-  var sessionQueue: dispatch_queue_t!
-  
+  var videoFilter: CoreImageVideoFilter?
   var detector: CIDetector?
   
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
-    videoDisplayView = GLKView(frame: view.bounds, context: EAGLContext(API: .OpenGLES2))
-    videoDisplayView.transform = CGAffineTransformMakeRotation(CGFloat(M_PI_2))
-    videoDisplayView.frame = view.bounds
-    view.addSubview(videoDisplayView)
     
-    renderContext = CIContext(EAGLContext: videoDisplayView.context)
-    sessionQueue = dispatch_queue_create("AVSessionQueue", DISPATCH_QUEUE_SERIAL)
-    
-    videoDisplayView.bindDrawable()
-    videoDisplayViewBounds = CGRect(x: 0, y: 0, width: videoDisplayView.drawableWidth, height: videoDisplayView.drawableHeight)
+    // Create the video filter
+    videoFilter = CoreImageVideoFilter(superview: view, applyFilterCallback: {
+      image in
+        return self.performDetection(image)
+      })
     
     // Start the video capture process
-    start()
+    videoFilter?.startFiltering()
   }
   
-  deinit {
-    dispatch_release(sessionQueue)
-  }
-  
-  func start() {
-    // Input from video camera
-    let device = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
-    var error: NSError?
-    let input = AVCaptureDeviceInput(device: device, error: &error)
-
-    // Start out with low quality
-    avSession = AVCaptureSession()
-    avSession.sessionPreset = AVCaptureSessionPresetMedium
-    
-    // Output
-    let videoOutput = AVCaptureVideoDataOutput()
-    
-    videoOutput.videoSettings = [ kCVPixelBufferPixelFormatTypeKey: kCVPixelFormatType_32BGRA]
-    videoOutput.alwaysDiscardsLateVideoFrames = true
-    videoOutput.setSampleBufferDelegate(self, queue: sessionQueue)
-    
-    // Join it all together
-    avSession.addInput(input)
-    avSession.addOutput(videoOutput)
-    
-    // And kick it off
-    avSession.startRunning()
-  }
   
   func performDetection(image: CIImage) -> CIImage? {
     var resultImage: CIImage?
@@ -108,52 +64,5 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
     let options = [CIDetectorAccuracy: CIDetectorAccuracyHigh, CIDetectorAspectRatio: 1.0]
     return CIDetector(ofType: CIDetectorTypeRectangle, context: nil, options: options)
   }
-  
-  //MARK: <AVCaptureVideoDataOutputSampleBufferDelegate
-  func captureOutput(captureOutput: AVCaptureOutput!, didOutputSampleBuffer sampleBuffer: CMSampleBuffer!, fromConnection connection: AVCaptureConnection!) {
-    
-    // Need to shimmy this through type-hell
-    let opaqueBuffer = CMSampleBufferGetImageBuffer(sampleBuffer).toOpaque()
-    let imageBuffer = Unmanaged<CVPixelBuffer>.fromOpaque(opaqueBuffer).takeUnretainedValue()
-    let sourceImage = CIImage(CVPixelBuffer: imageBuffer, options: nil)
-    
-    // Do some detection on the image
-    let detectionResult = performDetection(sourceImage)
-    var outputImage = sourceImage
-    if detectionResult {
-      outputImage = detectionResult!
-    }
-    
-    // Do some clipping
-    var drawFrame = outputImage.extent()
-    let imageAR = drawFrame.width / drawFrame.height
-    let viewAR = videoDisplayViewBounds.width / videoDisplayViewBounds.height
-    if imageAR > viewAR {
-      drawFrame.origin.x += (drawFrame.width - drawFrame.height * viewAR) / 2.0
-      drawFrame.size.width = drawFrame.height / viewAR
-    } else {
-      drawFrame.origin.y += (drawFrame.height - drawFrame.width / viewAR) / 2.0
-      drawFrame.size.height = drawFrame.width / viewAR
-    }
-    
-    videoDisplayView.bindDrawable()
-    if videoDisplayView.context != EAGLContext.currentContext() {
-      EAGLContext.setCurrentContext(videoDisplayView.context)
-    }
-    
-    // clear eagl view to grey
-    glClearColor(0.5, 0.5, 0.5, 1.0);
-    glClear(0x00004000)
-    
-    // set the blend mode to "source over" so that CI will use that
-    glEnable(0x0BE2);
-    glBlendFunc(1, 0x0303);
-    
-    renderContext.drawImage(outputImage, inRect: videoDisplayViewBounds, fromRect: drawFrame)
-    
-    videoDisplayView.display()
-    
-  }
-
 }
 
