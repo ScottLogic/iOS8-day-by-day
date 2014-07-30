898e662ea5dfdbe731006ad894b55f2b7280a36e
Sam Davies <sdavies@shinobicontrols.com>
2014-07-30 11:51:06 +0100

    Remove boilerplate

--- 13-coreimage-detectors/LiveDetection/LiveDetection/ViewController.swift
+++ 13-coreimage-detectors/LiveDetection/LiveDetection/ViewController.swift
@@ -22,11 +22,5 @@ class ViewController: UIViewController {
     // Do any additional setup after loading the view, typically from a nib.
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-
 }
 
