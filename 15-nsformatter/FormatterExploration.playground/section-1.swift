9396c3db80874bde7e54716a1ddac279d74a4270
Sam Davies <sdavies@shinobicontrols.com>
2014-07-30 07:42:19 +0100

    Demos for date components formatter and interval formatter

--- 15-nsformatter/FormatterExploration.playground/section-1.swift
+++ 15-nsformatter/FormatterExploration.playground/section-1.swift
@@ -1,2 +1,35 @@
 import UIKit
 
+//MARK: NSDateComponentsFormatter
+let dateComponentsFormatter = NSDateComponentsFormatter()
+let components = NSDateComponents()
+components.hour   = 2
+components.minute = 45
+
+let dcfStyles: [NSDateComponentsFormatterUnitsStyle] =
+  [.Positional, .Abbreviated, .Short, .Full, .SpellOut]
+for style in dcfStyles {
+  dateComponentsFormatter.unitsStyle = style
+  dateComponentsFormatter.stringFromDateComponents(components)
+}
+let now = NSDate()
+let longTimeAgo = NSDate(timeIntervalSince1970: 0.0)
+dateComponentsFormatter.unitsStyle = .Short
+dateComponentsFormatter.stringFromDate(longTimeAgo, toDate: now)
+
+dateComponentsFormatter.includesApproximationPhrase = true
+dateComponentsFormatter.stringFromDateComponents(components)
+dateComponentsFormatter.includesTimeRemainingPhrase = true
+dateComponentsFormatter.stringFromDateComponents(components)
+
+//MARK: NSDateIntervalFormatter
+let dateIntervalFormatter = NSDateIntervalFormatter()
+let difStyles: [NSDateIntervalFormatterStyle] =
+  [.NoStyle, .ShortStyle, .MediumStyle, .LongStyle, .FullStyle]
+for style in difStyles {
+  dateIntervalFormatter.dateStyle = style
+  dateIntervalFormatter.timeStyle = style
+  dateIntervalFormatter.stringFromDate(longTimeAgo, toDate: now)
+}
+
+
