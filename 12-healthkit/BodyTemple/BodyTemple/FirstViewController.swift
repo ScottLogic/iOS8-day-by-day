4afb7679dcbd5e2a7871e828a2ca309004b91319
Sam Davies <sdavies@shinobicontrols.com>
2014-08-01 09:19:11 +0100

    Create a tab-bar template app for BodyTemple

---
+++ 12-healthkit/BodyTemple/BodyTemple/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  BodyTemple
+//
+//  Created by Sam Davies on 01/08/2014.
+//  Copyright (c) 2014 ShinobiControls. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+                            
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
---
+++ 12-healthkit/BodyTemple/BodyTemple/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  BodyTemple
+//
+//  Created by Sam Davies on 01/08/2014.
+//  Copyright (c) 2014 ShinobiControls. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+                            
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
