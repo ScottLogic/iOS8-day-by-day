6a02414774e8ed5adca6ed0e3000af15c7eeb9eb
Sam Davies <sdavies@shinobicontrols.com>
2014-08-01 09:22:01 +0100

    Removing boiler plate and make it iPhone only
    HealthKit doesn't work on an iPad

--- 12-healthkit/BodyTemple/BodyTemple/FirstViewController.swift
+++ 12-healthkit/BodyTemple/BodyTemple/FirstViewController.swift
@@ -15,11 +15,5 @@ class FirstViewController: UIViewController {
     // Do any additional setup after loading the view, typically from a nib.
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-
 }
 
--- 12-healthkit/BodyTemple/BodyTemple/SecondViewController.swift
+++ 12-healthkit/BodyTemple/BodyTemple/SecondViewController.swift
@@ -14,12 +14,5 @@ class SecondViewController: UIViewController {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-
 }
 
