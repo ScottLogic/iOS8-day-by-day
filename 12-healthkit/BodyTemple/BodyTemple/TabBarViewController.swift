9aeede9faaa670514ac9173b221658c05aaa3bc0
Sam Davies <sdavies@shinobicontrols.com>
2014-08-01 13:29:32 +0100

    Wired up BodyTemple to HealthKit to allow saving of data

---
+++ 12-healthkit/BodyTemple/BodyTemple/TabBarViewController.swift
@@ -0,0 +1,72 @@
+//
+// Copyright 2014 Scott Logic
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import UIKit
+import HealthKit
+
+class TabBarViewController: UITabBarController {
+
+  var healthStore: HKHealthStore?
+  
+  init(coder aDecoder: NSCoder!) {
+    super.init(coder: aDecoder)
+    createAndPropogateHealthStore()
+  }
+  
+  init(nibName nibNameOrNil: String!, bundle nibBundleOrNil: NSBundle!) {
+    super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    createAndPropogateHealthStore()
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    requestAuthorisationForHealthStore()
+  }
+  
+  private func createAndPropogateHealthStore() {
+    if !self.healthStore {
+      self.healthStore = HKHealthStore()
+    }
+    
+    for vc in viewControllers {
+      if let healthStoreUser = vc as? HealthStoreUser {
+        healthStoreUser.healthStore = self.healthStore
+      }
+    }
+  }
+  
+  private func requestAuthorisationForHealthStore() {
+    let dataTypesToWrite = [
+      HKQuantityType.quantityTypeForIdentifier(HKQuantityTypeIdentifierBodyMass)
+    ]
+    let dataTypesToRead = [
+      HKQuantityType.quantityTypeForIdentifier(HKQuantityTypeIdentifierBodyMass),
+      HKQuantityType.quantityTypeForIdentifier(HKQuantityTypeIdentifierHeight),
+      HKQuantityType.quantityTypeForIdentifier(HKQuantityTypeIdentifierBodyMassIndex)
+    ]
+    
+    self.healthStore?.requestAuthorizationToShareTypes(NSSet(array: dataTypesToWrite),
+      readTypes: NSSet(array: dataTypesToRead), completion: {
+      (success, error) in
+        if success {
+          println("Permissions granted")
+        } else {
+          println("There was a problem with permissions. The app will not work as expected. \(error)")
+        }
+      })
+  }
+}
--- 12-healthkit/BodyTemple/BodyTemple/WeightTableViewController.swift
+++ 12-healthkit/BodyTemple/BodyTemple/WeightTableViewController.swift
@@ -17,16 +17,12 @@
 import UIKit
 import HealthKit
 
-class WeightTableViewController: UITableViewController, WeightEntryDelegate {
+class WeightTableViewController: UITableViewController, WeightEntryDelegate, HealthStoreUser {
+  
+  var healthStore: HKHealthStore?
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    
-    // Uncomment the following line to preserve selection between presentations
-    // self.clearsSelectionOnViewWillAppear = false
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     title = "Weight History"
   }
   
@@ -43,7 +39,6 @@ class WeightTableViewController: UITableViewController, WeightEntryDelegate {
     // Return the number of rows in the section.
     return 0
   }
-  
   /*
   override func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
   let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as UITableViewCell
@@ -52,42 +47,7 @@ class WeightTableViewController: UITableViewController, WeightEntryDelegate {
   
   return cell
   }
-  */
-  
-  /*
-  // Override to support conditional editing of the table view.
-  override func tableView(tableView: UITableView!, canEditRowAtIndexPath indexPath: NSIndexPath!) -> Bool {
-  // Return NO if you do not want the specified item to be editable.
-  return true
-  }
-  */
-  
-  /*
-  // Override to support editing the table view.
-  override func tableView(tableView: UITableView!, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath!) {
-  if editingStyle == .Delete {
-  // Delete the row from the data source
-  tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-  } else if editingStyle == .Insert {
-  // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-  }
-  }
-  */
-  
-  /*
-  // Override to support rearranging the table view.
-  override func tableView(tableView: UITableView!, moveRowAtIndexPath fromIndexPath: NSIndexPath!, toIndexPath: NSIndexPath!) {
-  
-  }
-  */
-  
-  /*
-  // Override to support conditional rearranging of the table view.
-  override func tableView(tableView: UITableView!, canMoveRowAtIndexPath indexPath: NSIndexPath!) -> Bool {
-  // Return NO if you do not want the item to be re-orderable.
-  return true
-  }
-  */
+*/
   
   // MARK: - Navigation
   
@@ -104,7 +64,15 @@ class WeightTableViewController: UITableViewController, WeightEntryDelegate {
   
   // MARK: - WeightEntryDelegate
   func weightEntryDidComplete(weight: HKQuantitySample) {
-    println(weight)
+    println("Saving weight")
+    self.healthStore?.saveObject(weight, withCompletion: {
+      (success, error) in
+      if success {
+        println("Weight saved successfully 😃")
+      } else {
+        println("Error: \(error)")
+      }
+      })
   }
   
 }
