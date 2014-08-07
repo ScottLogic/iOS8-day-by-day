e93c3dbab0cea34ad6a9ea50c8643b00dafd86b0
Sam Davies <sdavies@shinobicontrols.com>
2014-08-07 18:21:23 +0100

    Allow live fiddling with the chromakey threshold
    It's a bit slow in the simulator, but quite cool :)

--- 19-core-image-kernels/FilterBuilder/FilterBuilder/ChromaKeyViewController.swift
+++ 19-core-image-kernels/FilterBuilder/FilterBuilder/ChromaKeyViewController.swift
@@ -18,25 +18,45 @@ import UIKit
 
 class ChromaKeyViewController: UIViewController {
   
+  // MARK: - Properties
+  let filter = ChromaKeyFilter()
+  
+  // MARK: - IBOutlets
   @IBOutlet weak var outputImageView: UIImageView!
-                            
+  @IBOutlet weak var thresholdSlider: UISlider!
+  
+  // MARK: - Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
-    outputImageView.image = self.filteredImage()
+    setupFilter()
+    updateOutputImage()
+  }
+  
+  // MARK: - IBActions
+  @IBAction func handleThresholdSliderChanged(sender: UISlider) {
+    if abs(filter.threshold - CGFloat(sender.value)) > 0.05 {
+      updateOutputImage()
+    }
+  }
+  
+  // MARK: - Utility methods
+  private func updateOutputImage() {
+    filter.threshold = CGFloat(thresholdSlider.value)
+    outputImageView.image = filteredImage()
   }
   
   private func filteredImage() -> UIImage {
-    let filter = ChromaKeyFilter()
-    
+    let outputImage = filter.outputImage
+    return UIImage(CIImage: outputImage)
+  }
+  
+  private func setupFilter() {
     // Image (C) Cristian Borquez
     // https://flic.kr/p/7hhXk1
     let inputImage = UIImage(named: "chroma_key")
-    
     filter.inputImage = CIImage(image: inputImage)
     filter.activeColor = CIColor(red: 0, green: 1, blue: 0)
-    let outputImage = filter.outputImage
-    return UIImage(CIImage: outputImage, scale: inputImage.scale, orientation: inputImage.imageOrientation)
   }
 
 }
