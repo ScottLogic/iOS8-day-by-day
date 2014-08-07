39d21366ea168819838a70034c3b8b6834708a5f
Sam Davies <sdavies@shinobicontrols.com>
2014-08-07 15:36:29 +0100

    Doesn't actually work because CIImage isn't liked in playgrounds

--- 19-core-image-kernels/CoreImageKernels.playground/section-1.swift
+++ 19-core-image-kernels/CoreImageKernels.playground/section-1.swift
@@ -1,5 +1,57 @@
-// Playground - noun: a place where people can play
-
 import UIKit
+import CoreImage
 
-var str = "Hello, playground"
+// MARK: - ChromaKey Filter
+// MARK: -
+
+class ChromaKeyFilter: CIFilter {
+  // MARK: - Properties
+  var kernel: CIColorKernel?
+  var inputImage: CIImage?
+  var activeColor: CIColor?
+  
+  // MARK: - Initialization
+  override init() {
+    super.init()
+    kernel = createKernel()
+  }
+  
+  required init(coder aDecoder: NSCoder!) {
+    super.init(coder: aDecoder)
+    kernel = createKernel()
+  }
+  
+  // MARK: - API
+  func outputImage() -> CIImage? {
+    if let inputImage = inputImage {
+      let dod = inputImage.extent()
+      if let kernel = kernel {
+        var args = [inputImage as AnyObject]
+        return kernel.applyWithExtent(dod, arguments: args)
+      }
+    }
+    return nil
+  }
+  
+  // MARK: - Utility methods
+  private func createKernel() -> CIColorKernel {
+    let kernelString =
+    "kernel vec4 passThrough( __sample s) { " +
+    "  return s.rgba; " +
+    "}"
+    return CIColorKernel(string: kernelString)
+  }
+}
+
+let context = CIContext(options: nil)
+let inputImage = UIImage(named: "chroma_key_sample.jpg")
+let chromaKeyFilter = ChromaKeyFilter()
+let ciimage = CIImage.emptyImage()
+
+let ciInputImage = CIImage(CGImage: inputImage.CGImage)
+chromaKeyFilter.inputImage = ciInputImage
+
+
+
+
+
