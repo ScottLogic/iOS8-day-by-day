4423c5467d85e3095323bdb52dae31230cd89e33
Sam Davies <sdavies@shinobicontrols.com>
2014-08-13 13:15:56 +0100

    Perform trait override to demo split VC on an iPhone

--- 18-split-view-controller/NinjaWeapons/NinjaWeapons/AppDelegate.swift
+++ 18-split-view-controller/NinjaWeapons/NinjaWeapons/AppDelegate.swift
@@ -17,33 +17,13 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate {
   
   var window: UIWindow?
   
   func application(application: UIApplication!, didFinishLaunchingWithOptions launchOptions: NSDictionary!) -> Bool {
     // Override point for customization after application launch.
-    let splitViewController = self.window!.rootViewController as UISplitViewController
-    let navigationController = splitViewController.viewControllers[splitViewController.viewControllers.count-1] as UINavigationController
-    navigationController.topViewController.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem()
-    splitViewController.delegate = self
-
     return true
   }
-  
-  // MARK: - Split view
-  
-  func splitViewController(splitViewController: UISplitViewController!, collapseSecondaryViewController secondaryViewController:UIViewController!, ontoPrimaryViewController primaryViewController:UIViewController!) -> Bool {
-    if let secondaryAsNavController = secondaryViewController as? UINavigationController {
-      if let topAsDetailController = secondaryAsNavController.topViewController as? DetailViewController {
-        if topAsDetailController.weapon == nil {
-          // Return true to indicate that we have handled the collapse by doing nothing; the secondary controller will be discarded.
-          return true
-        }
-      }
-    }
-    return false
-  }
-  
 }
 
---
+++ 18-split-view-controller/NinjaWeapons/NinjaWeapons/TraitOverrideViewController.swift
@@ -0,0 +1,58 @@
+//
+// Copyright 2014 Scott Logic
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import UIKit
+
+class TraitOverrideViewController: UIViewController, UISplitViewControllerDelegate {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    performTraitCollectionOverrideForSize(view.bounds.size)
+    // Set up split view delegate
+    let splitVC = self.childViewControllers[0] as UISplitViewController
+    splitVC.delegate = self
+    let navVC = splitVC.childViewControllers.last as UINavigationController
+    navVC.topViewController.navigationItem.leftBarButtonItem = splitVC.displayModeButtonItem()
+  }
+  
+  override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator!) {
+    performTraitCollectionOverrideForSize(size)
+  }
+  
+  private func performTraitCollectionOverrideForSize(size: CGSize) {
+    var overrideTraitCollection: UITraitCollection? = nil
+    if size.width > 320 {
+      overrideTraitCollection = UITraitCollection(horizontalSizeClass: .Regular)
+    }
+    for vc in self.childViewControllers as [UIViewController] {
+      setOverrideTraitCollection(overrideTraitCollection, forChildViewController: vc)
+    }
+  }
+  
+  
+  // MARK: - Split view
+  func splitViewController(splitViewController: UISplitViewController!, collapseSecondaryViewController secondaryViewController:UIViewController!, ontoPrimaryViewController primaryViewController:UIViewController!) -> Bool {
+    if let secondaryAsNavController = secondaryViewController as? UINavigationController {
+      if let topAsDetailController = secondaryAsNavController.topViewController as? DetailViewController {
+        if topAsDetailController.weapon == nil {
+          // Return true to indicate that we have handled the collapse by doing nothing; the secondary controller will be discarded.
+          return true
+        }
+      }
+    }
+    return false
+  }
+}
