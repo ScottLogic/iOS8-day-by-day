31ea1b8fd56da44f01c9b39085c5c20a69e9f18f
Sam Davies <sdavies@shinobicontrols.com>
2014-08-13 09:24:51 +0100

    Remove storyboard ambiguity, and wire weapons to the master VC

--- 18-split-view-controller/NinjaWeapons/NinjaWeapons/MasterViewController.swift
+++ 18-split-view-controller/NinjaWeapons/NinjaWeapons/MasterViewController.swift
@@ -17,10 +17,8 @@
 import UIKit
 
 class MasterViewController: UITableViewController {
-  
-  var detailViewController: DetailViewController? = nil
-  var objects = NSMutableArray()
-  
+
+  var weaponProvider: WeaponProvider? = nil
   
   override func awakeFromNib() {
     super.awakeFromNib()
@@ -28,33 +26,11 @@ class MasterViewController: UITableViewController {
       self.clearsSelectionOnViewWillAppear = false
       self.preferredContentSize = CGSize(width: 320.0, height: 600.0)
     }
+    weaponProvider = WeaponProvider()
   }
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-    self.navigationItem.leftBarButtonItem = self.editButtonItem()
-    
-    let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
-    self.navigationItem.rightBarButtonItem = addButton
-    if let split = self.splitViewController {
-      let controllers = split.viewControllers
-      self.detailViewController = controllers[controllers.count-1].topViewController as? DetailViewController
-    }
-  }
-  
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-  
-  func insertNewObject(sender: AnyObject) {
-    if objects == nil {
-      objects = NSMutableArray()
-    }
-    objects.insertObject(NSDate.date(), atIndex: 0)
-    let indexPath = NSIndexPath(forRow: 0, inSection: 0)
-    self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
   }
   
   // MARK: - Segues
@@ -62,11 +38,12 @@ class MasterViewController: UITableViewController {
   override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
     if segue.identifier == "showDetail" {
       let indexPath = self.tableView.indexPathForSelectedRow()
-      let object = objects[indexPath.row] as NSDate
-      let controller = (segue.destinationViewController as UINavigationController).topViewController as DetailViewController
-      controller.detailItem = object
-      controller.navigationItem.leftBarButtonItem = self.splitViewController.displayModeButtonItem()
-      controller.navigationItem.leftItemsSupplementBackButton = true
+      if let weapon = weaponProvider?.weapons[indexPath.row] {
+        let controller = (segue.destinationViewController as UINavigationController).topViewController as DetailViewController
+        //controller.detailItem = weapon
+        controller.navigationItem.leftBarButtonItem = self.splitViewController.displayModeButtonItem()
+        controller.navigationItem.leftItemsSupplementBackButton = true
+      }
     }
   }
   
@@ -77,31 +54,15 @@ class MasterViewController: UITableViewController {
   }
   
   override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return objects.count
+    return weaponProvider?.weapons.count ?? 0
   }
   
   override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
     
-    let object = objects[indexPath.row] as NSDate
-    cell.textLabel.text = object.description
+    let weapon = weaponProvider?.weapons[indexPath.row]
+    cell.textLabel.text = weapon?.name
     return cell
   }
-  
-  override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-    // Return false if you do not want the specified item to be editable.
-    return true
-  }
-  
-  override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-    if editingStyle == .Delete {
-      objects.removeObjectAtIndex(indexPath.row)
-      tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-    } else if editingStyle == .Insert {
-      // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
-    }
-  }
-  
-  
 }
 
