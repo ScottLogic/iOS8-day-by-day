e6ad9a08822492acb439d199d0e164777887eff0
Sam Davies <sdavies@shinobicontrols.com>
2014-08-28 15:57:52 -0600

    Re-indent

--- 23-photo-extension/ChromaKey/ChromaKeyExtension/PhotoEditingViewController.swift
+++ 23-photo-extension/ChromaKey/ChromaKeyExtension/PhotoEditingViewController.swift
@@ -19,63 +19,63 @@ import Photos
 import PhotosUI
 
 class PhotoEditingViewController: UIViewController, PHContentEditingController {
-
-    var input: PHContentEditingInput?
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+  
+  var input: PHContentEditingInput?
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  // MARK: - PHContentEditingController
+  
+  func canHandleAdjustmentData(adjustmentData: PHAdjustmentData?) -> Bool {
+    // Inspect the adjustmentData to determine whether your extension can work with past edits.
+    // (Typically, you use its formatIdentifier and formatVersion properties to do this.)
+    return false
+  }
+  
+  func startContentEditingWithInput(contentEditingInput: PHContentEditingInput?, placeholderImage: UIImage) {
+    // Present content for editing, and keep the contentEditingInput for use when closing the edit session.
+    // If you returned YES from canHandleAdjustmentData:, contentEditingInput has the original image and adjustment data.
+    // If you returned NO, the contentEditingInput has past edits "baked in".
+    input = contentEditingInput
+  }
+  
+  func finishContentEditingWithCompletionHandler(completionHandler: ((PHContentEditingOutput!) -> Void)!) {
+    // Update UI to reflect that editing has finished and output is being rendered.
+    
+    // Render and provide output on a background queue.
+    dispatch_async(dispatch_get_global_queue(CLong(DISPATCH_QUEUE_PRIORITY_DEFAULT), 0)) {
+      // Create editing output from the editing input.
+      let output = PHContentEditingOutput(contentEditingInput: self.input)
+      
+      // Provide new adjustments and render output to given location.
+      // output.adjustmentData = <#new adjustment data#>
+      // let renderedJPEGData = <#output JPEG#>
+      // renderedJPEGData.writeToURL(output.renderedContentURL, atomically: true)
+      
+      // Call completion handler to commit edit to Photos.
+      completionHandler?(output)
+      
+      // Clean up temporary files, etc.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    // MARK: - PHContentEditingController
-
-    func canHandleAdjustmentData(adjustmentData: PHAdjustmentData?) -> Bool {
-        // Inspect the adjustmentData to determine whether your extension can work with past edits.
-        // (Typically, you use its formatIdentifier and formatVersion properties to do this.)
-        return false
-    }
-
-    func startContentEditingWithInput(contentEditingInput: PHContentEditingInput?, placeholderImage: UIImage) {
-        // Present content for editing, and keep the contentEditingInput for use when closing the edit session.
-        // If you returned YES from canHandleAdjustmentData:, contentEditingInput has the original image and adjustment data.
-        // If you returned NO, the contentEditingInput has past edits "baked in".
-        input = contentEditingInput
-    }
-
-    func finishContentEditingWithCompletionHandler(completionHandler: ((PHContentEditingOutput!) -> Void)!) {
-        // Update UI to reflect that editing has finished and output is being rendered.
-        
-        // Render and provide output on a background queue.
-        dispatch_async(dispatch_get_global_queue(CLong(DISPATCH_QUEUE_PRIORITY_DEFAULT), 0)) {
-            // Create editing output from the editing input.
-            let output = PHContentEditingOutput(contentEditingInput: self.input)
-            
-            // Provide new adjustments and render output to given location.
-            // output.adjustmentData = <#new adjustment data#>
-            // let renderedJPEGData = <#output JPEG#>
-            // renderedJPEGData.writeToURL(output.renderedContentURL, atomically: true)
-            
-            // Call completion handler to commit edit to Photos.
-            completionHandler?(output)
-            
-            // Clean up temporary files, etc.
-        }
-    }
-
-    var shouldShowCancelConfirmation: Bool {
-        // Determines whether a confirmation to discard changes should be shown to the user on cancel.
-        // (Typically, this should be "true" if there are any unsaved changes.)
-        return false
-    }
-
-    func cancelContentEditing() {
-        // Clean up temporary files, etc.
-        // May be called after finishContentEditingWithCompletionHandler: while you prepare output.
-    }
-
+  }
+  
+  var shouldShowCancelConfirmation: Bool {
+    // Determines whether a confirmation to discard changes should be shown to the user on cancel.
+    // (Typically, this should be "true" if there are any unsaved changes.)
+    return false
+  }
+  
+  func cancelContentEditing() {
+    // Clean up temporary files, etc.
+    // May be called after finishContentEditingWithCompletionHandler: while you prepare output.
+  }
+  
 }
