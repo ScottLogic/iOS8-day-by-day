2b447624cd246ba3ca89e8ab84c9aeb3cc22816f
Sam Davies <sdavies@shinobicontrols.com>
2014-08-28 12:53:15 -0600

    Create blank single-page app to demo presentation controllers

---
+++ 24-presentation-controllers/BouncyPresent/BouncyPresent/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BouncyPresent
+//
+//  Created by Sam Davies on 28/08/2014.
+//  Copyright (c) 2014 ShinobiControls. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
