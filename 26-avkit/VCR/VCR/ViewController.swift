8612b9260a05b4e42977565a64b677b371620202
Sam Davies <sdavies@shinobicontrols.com>
2014-09-08 13:36:11 +0700

    Create single view app to demo the new AVKit framework

---
+++ 26-avkit/VCR/VCR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VCR
+//
+//  Created by Sam Davies on 08/09/2014.
+//  Copyright (c) 2014 ShinobiControls. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
