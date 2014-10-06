bdc4ebeb972cabdc804421883042523ac586ea19
Sam Davies <sdavies@shinobicontrols.com>
2014-10-06 22:02:38 +0100

    Update some of the UI
    This is nearly done now :|

--- 33-cloudkit/CloudNotes/CloudNotes/DetailViewController.swift
+++ 33-cloudkit/CloudNotes/CloudNotes/DetailViewController.swift
@@ -15,13 +15,16 @@
 //
 
 import UIKit
+import MapKit
 
 class DetailViewController: UIViewController {
 
-  @IBOutlet weak var detailDescriptionLabel: UILabel!
-
-
-  var detailItem: AnyObject? {
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var contentLabel: UILabel!
+  @IBOutlet weak var mapView: MKMapView!
+  
+  
+  var note: Note? {
     didSet {
         // Update the view.
         self.configureView()
@@ -30,10 +33,9 @@ class DetailViewController: UIViewController {
 
   func configureView() {
     // Update the user interface for the detail item.
-    if let detail: AnyObject = self.detailItem {
-        if let label = self.detailDescriptionLabel {
-            label.text = detail.description
-        }
+    if let note = note {
+      titleLabel.text = note.title
+      contentLabel.text = note.content 
     }
   }
 
@@ -42,12 +44,6 @@ class DetailViewController: UIViewController {
     // Do any additional setup after loading the view, typically from a nib.
     self.configureView()
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
 
 }
 
--- 33-cloudkit/CloudNotes/CloudNotes/MasterViewController.swift
+++ 33-cloudkit/CloudNotes/CloudNotes/MasterViewController.swift
@@ -15,12 +15,17 @@
 //
 
 import UIKit
+import CloudKit
 
 class MasterViewController: UITableViewController {
 
   var detailViewController: DetailViewController? = nil
-  var objects = NSMutableArray()
-
+  let noteManager = CloudKitNoteManager(database: CKContainer.defaultContainer().privateCloudDatabase)
+  var noteCollection: [Note]? {
+    didSet {
+      tableView.reloadData()
+    }
+  }
 
   override func awakeFromNib() {
     super.awakeFromNib()
@@ -43,28 +48,25 @@ class MasterViewController: UITableViewController {
     }
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
 
   func insertNewObject(sender: AnyObject) {
-    objects.insertObject(NSDate.date(), atIndex: 0)
-    let indexPath = NSIndexPath(forRow: 0, inSection: 0)
-    self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+    //objects.insertObject(NSDate.date(), atIndex: 0)
+    //let indexPath = NSIndexPath(forRow: 0, inSection: 0)
+    //self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
   }
 
   // MARK: - Segues
 
   override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
     if segue.identifier == "showDetail" {
-        if let indexPath = self.tableView.indexPathForSelectedRow() {
-            let object = objects[indexPath.row] as NSDate
-            let controller = (segue.destinationViewController as UINavigationController).topViewController as DetailViewController
-            controller.detailItem = object
-            controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
-            controller.navigationItem.leftItemsSupplementBackButton = true
+      if let indexPath = self.tableView.indexPathForSelectedRow() {
+        if let note = noteCollection?[indexPath.row] {
+          let controller = (segue.destinationViewController as UINavigationController).topViewController as DetailViewController
+          controller.note = note
+          controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
+          controller.navigationItem.leftItemsSupplementBackButton = true
         }
+      }
     }
   }
 
@@ -75,31 +77,17 @@ class MasterViewController: UITableViewController {
   }
 
   override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return objects.count
+    return noteCollection?.count ?? 0
   }
 
   override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
 
-    let object = objects[indexPath.row] as NSDate
-    cell.textLabel?.text = object.description
+    if let note = noteCollection?[indexPath.row] {
+      cell.textLabel?.text = note.title
+    }
     return cell
   }
-
-  override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-    // Return false if you do not want the specified item to be editable.
-    return true
-  }
-
-  override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-    if editingStyle == .Delete {
-        objects.removeObjectAtIndex(indexPath.row)
-        tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-    } else if editingStyle == .Insert {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
-    }
-  }
-
 
 }
 
