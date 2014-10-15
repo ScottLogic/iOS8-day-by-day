c938c60c2ecb22cd071f7b85a52fba016ab7f1af
Sam Davies <sdavies@shinobicontrols.com>
2014-10-15 10:56:15 +0100

    Create sample single-page app to demo core location auth

---
+++ 34-corelocation-auth/WhereAmI/WhereAmI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhereAmI
+//
+//  Created by sam on 15/10/2014.
+//  Copyright (c) 2014 ShinobiControls. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
