b536ba53a9b98787fe96a2e6b8858b65f19390dc
Sam Davies <sdavies@shinobicontrols.com>
2014-10-16 11:41:23 +0100

    Create sample project to demo CoreMotion

---
+++ 35-coremotion/LocoMotion/LocoMotion/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  LocoMotion
+//
+//  Created by sam on 16/10/2014.
+//  Copyright (c) 2014 ShinobiControls. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
