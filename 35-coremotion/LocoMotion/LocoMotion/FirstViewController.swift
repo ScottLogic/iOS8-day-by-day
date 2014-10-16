a380bd906f675967c0c4ec1bc11626d9783894fb
Sam Davies <sdavies@shinobicontrols.com>
2014-10-16 12:08:31 +0100

    Removed boilerplate

--- 35-coremotion/LocoMotion/LocoMotion/FirstViewController.swift
+++ 35-coremotion/LocoMotion/LocoMotion/FirstViewController.swift
@@ -22,12 +22,5 @@ class FirstViewController: UIViewController {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-
 }
 
