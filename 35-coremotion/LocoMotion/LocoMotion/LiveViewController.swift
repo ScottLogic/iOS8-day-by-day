4a77169f1b6b3cec8fa02783bec406f6f1a6a26e
Sam Davies <sdavies@shinobicontrols.com>
2014-10-17 11:19:29 +0100

    Finished the live updating page of the app

--- 35-coremotion/LocoMotion/LocoMotion/LiveViewController.swift
+++ 35-coremotion/LocoMotion/LocoMotion/LiveViewController.swift
@@ -15,12 +15,75 @@
 //
 
 import UIKit
+import CoreMotion
 
 class LiveViewController: UIViewController {
-
+  
+  @IBOutlet weak var activityImageView: UIImageView!
+  @IBOutlet weak var stepsLabel: UILabel!
+  @IBOutlet weak var floorsLabel: UILabel!
+  @IBOutlet weak var distanceLabel: UILabel!
+  @IBOutlet weak var altitudeLabel: UILabel!
+  
+  let dataProcessingQueue = NSOperationQueue()
+  let pedometer = CMPedometer()
+  let altimeter = CMAltimeter()
+  let activityManager = CMMotionActivityManager()
+  
+  let lengthFormatter = NSLengthFormatter()
+  
+  var altChange: Double = 0
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    lengthFormatter.numberFormatter.usesSignificantDigits = false
+    lengthFormatter.numberFormatter.maximumSignificantDigits = 2
+    lengthFormatter.unitStyle = .Short
+    
+    
+    // Prepare altimeter
+    altimeter.startRelativeAltitudeUpdatesToQueue(dataProcessingQueue) {
+      (data, error) in
+      if error != nil {
+        println("There was an error obtaining altimeter data: \(error)")
+      } else {
+        dispatch_async(dispatch_get_main_queue()) {
+          self.altChange += data.relativeAltitude
+          self.altitudeLabel.text = "\(self.lengthFormatter.stringFromMeters(self.altChange))"
+        }
+      }
+    }
+    
+    // Prepare pedometer
+    pedometer.startPedometerUpdatesFromDate(NSDate()) {
+      (data, error) in
+      if error != nil {
+        println("There was an error obtainng pedometer data: \(error)")
+      } else {
+        dispatch_async(dispatch_get_main_queue()) {
+          self.floorsLabel.text = "\(data.floorsAscended)"
+          self.stepsLabel.text = "\(data.numberOfSteps)"
+          self.distanceLabel.text = "\(self.lengthFormatter.stringFromMeters(data.distance))"
+        }
+      }
+    }
+    
+    // Prepare activity updates
+    activityManager.startActivityUpdatesToQueue(dataProcessingQueue) {
+      data in
+      dispatch_async(dispatch_get_main_queue()) {
+        if data.running {
+          self.activityImageView.image = UIImage(named: "run")
+        } else if data.cycling {
+          self.activityImageView.image = UIImage(named: "cycle")
+        } else if data.walking {
+          self.activityImageView.image = UIImage(named: "walk")
+        } else {
+          self.activityImageView.image = nil
+        }
+      }
+    }
   }
 
 }
