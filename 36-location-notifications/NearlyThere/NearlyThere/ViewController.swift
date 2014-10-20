9fc1a2cce82eec7844597579f91f9cf88485973f
Sam Davies <sdavies@shinobicontrols.com>
2014-10-20 10:58:27 +0100

    License headers and removing boilerplate

--- 36-location-notifications/NearlyThere/NearlyThere/ViewController.swift
+++ 36-location-notifications/NearlyThere/NearlyThere/ViewController.swift
@@ -1,9 +1,16 @@
 //
-//  ViewController.swift
-//  NearlyThere
+// Copyright 2014 Scott Logic
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//  Created by sam on 20/10/2014.
-//  Copyright (c) 2014 ShinobiControls. All rights reserved.
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 //
 
 import UIKit
@@ -14,12 +21,6 @@ class ViewController: UIViewController {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
 
 }
 
