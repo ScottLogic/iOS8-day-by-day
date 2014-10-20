d33bdcd6f18fc99be1fd3886005132d7eea526a2
Sam Davies <sdavies@shinobicontrols.com>
2014-10-20 12:31:14 +0100

    Create location based notifications

--- 36-location-notifications/NearlyThere/NearlyThere/ViewController.swift
+++ 36-location-notifications/NearlyThere/NearlyThere/ViewController.swift
@@ -14,12 +14,50 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
+  @IBOutlet weak var mapView: MKMapView!
+  @IBOutlet weak var actionButton: UIButton!
+  
+  var annotation: MKPointAnnotation?
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    let notificationSettings = UIUserNotificationSettings(forTypes: UIUserNotificationType.Alert, categories: nil)
+    UIApplication.sharedApplication().registerUserNotificationSettings(notificationSettings)
+  
+  }
+
+  @IBAction func handleButtonPressed(sender: UIButton) {
+    // Painfully lazy and bad-style state machine
+    if actionButton.titleForState(.Normal) == "Drop Pin" {
+      annotation = MKPointAnnotation()
+      annotation?.coordinate = mapView.centerCoordinate
+      mapView.addAnnotation(annotation)
+      actionButton.setTitle("Notify Me", forState: .Normal)
+    } else if actionButton.titleForState(.Normal) == "Notify Me" {
+      // Create a notification
+      let notification = UILocalNotification()
+      notification.alertBody = "You're nearly there!"
+      notification.regionTriggersOnce = true
+      notification.region = CLCircularRegion(center: annotation!.coordinate, radius: 5000, identifier: "Destination")
+      UIApplication.sharedApplication().scheduleLocalNotification(notification)
+      
+      actionButton.setTitle("Cancel Notification", forState: .Normal)
+    } else if actionButton.titleForState(.Normal) == "Cancel Notification" {
+      UIApplication.sharedApplication().cancelAllLocalNotifications()
+      mapView.removeAnnotation(annotation)
+      actionButton.setTitle("Drop Pin", forState: .Normal)
+    }
+  }
+  
+  // MARK:- MKMapViewDelegate methods
+  func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
+    let pin = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "PinAnnotation")
+    pin.animatesDrop = true
+    return pin
   }
 
 }
