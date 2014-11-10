2563391d1679f042366f5aad44fa218ca9e829ec
Sam Davies <sdavies@shinobicontrols.com>
2014-11-10 16:30:47 +0000

    Adds a map view to the main view controller

--- 38-handoff/MapOff/MapOff/ViewController.swift
+++ 38-handoff/MapOff/MapOff/ViewController.swift
@@ -15,9 +15,12 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var mapView: MKMapView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
