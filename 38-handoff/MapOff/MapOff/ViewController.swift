efad33e3446b7ca6069d83efb880cc1f0312b7c9
Sam Davies <sdavies@shinobicontrols.com>
2014-11-10 16:05:06 +0000

    Creating template project to demo handoff

---
+++ 38-handoff/MapOff/MapOff/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapOff
+//
+//  Created by sam on 10/11/2014.
+//  Copyright (c) 2014 ShinobiControls. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
